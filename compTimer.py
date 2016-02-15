1b0f37b89c66be81cb8095e3fb37de480614956b
Tim Howard <tghoward@gmail.com>
2016-02-14 22:33:11 -0500

    looping with user input

--- compTimer.py
+++ compTimer.py
@@ -26,36 +26,36 @@ GPIO.setup(led2_gpio, GPIO.OUT)
 outfile = os.path.expanduser("~/LaserTimer/carTimes.txt")
 
 try: 
+	for i in range(5):
+		run = raw_input("Team and run number: ")
+		print "ready for " + run 
+		while True:
 
-	run = raw_input("Team and run number: ")
-	print "ready for " + run 
-	while True:
+			if (GPIO.input(a_pin) != gateState):
+				gateState = not gateState
+			
+				if (gateState == True):
+					crosses = crosses + 1
 
-		if (GPIO.input(a_pin) != gateState):
-			gateState = not gateState
+				if (gateState == True and crosses%2 == 1):
+					start = time.time()
+					print "first gate triggered" 
+					
+			if (GPIO.input(b_pin) != gateTwoState):
+				gateTwoState = not gateTwoState
 			
-			if (gateState == True):
-				crosses = crosses + 1
-
-			if (gateState == True and crosses%2 == 1):
-				start = time.time()
-				print "first gate triggered" 
-			
-		
-		if (GPIO.input(b_pin) != gateTwoState):
-			gateTwoState = not gateTwoState
-			
-			if (gateTwoState == True):
-				crosses = crosses + 1
+				if (gateTwoState == True):
+					crosses = crosses + 1
 	
-			if (gateTwoState == True and crosses%2 == 0):
-				stop = time.time()
-				print "second gate ..."
-				if stop - start > 0.01:
-					t = stop - start
-					print "... Time: ", t , "s"
-					with open(outfile, "a") as f:
-    						f.writelines("\n" + time.asctime() + "\n" + run + ": " + "\n" + str(t))
+				if (gateTwoState == True and crosses%2 == 0):
+					stop = time.time()
+					print "second gate ..."
+					if stop - start > 0.01:
+						t = stop - start
+						print "... Time: ", t , "s"
+						with open(outfile, "a") as f:
+    							f.writelines("\n" + time.asctime() + "\n" + run + ": " + str(t))
+						break
 
 		if (GPIO.input(a_pin) == True):
 			GPIO.output(led_gpio, False)
@@ -67,7 +67,11 @@ try:
 		else:
 			GPIO.output(led2_gpio, True)	
 		
+	
+	GPIO.cleanup()
 
 except KeyboardInterrupt: 
 	GPIO.cleanup()	
+
+
 
